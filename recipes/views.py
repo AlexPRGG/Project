fedb33e4cb47f82a1a5948bdc3e42604f6d7a3ab
Álex Tércio <alextercio2018@gmail.com>
2023-08-22 15:19:45 -0300

    Ex. Templates Temp

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Álex Tércio'
+    })
 
 def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'temp/temp.html')
 
 def sobre(request):
     return HttpResponse('SOBRE')
